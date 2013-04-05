0d59afc3dbaa51718090e5bf0d1710e463795340
Apprentice <apprentice@devbootcamp.com>
2013-04-05 15:00:38 -0700

    added the game post route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,17 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+
+get '/game/:id' do
+
+  erb :game_view
+end
+
+
+post '/game/:deck' do
+  @game = current_user.games.create
+  @game.decks << params[:deck]
+
+  erb :game_view
+end
